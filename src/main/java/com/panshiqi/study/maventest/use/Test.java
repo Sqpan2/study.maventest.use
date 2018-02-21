12ceb1262dd4ea2a6ea51ccd69d7f057b39688e2
Sqpan2 <951514058@qq.com>
2018-02-21 22:39:41 +0800

    study.maventest.use
    使用github搭建maven私库，这个工程模拟jar包依赖

---
+++ src/main/java/com/panshiqi/study/maventest/use/Test.java
@@ -0,0 +1,22 @@
+/**
+ * @company   : com.sqpan2.study
+ * @poroject   : sqpan2 study
+ * @copyright : copyright (c) 2017
+ * 
+ * @description: TODO
+ * @author   : Sqpan2
+ * @date     : 2018年2月7日 下午10:41:44
+ * @version  : v1.0
+ */
+package com.panshiqi.study.maventest.use;
+
+import com.panshiqi.study.maventest.TestUtil;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		TestUtil.printHello();
+	}
+
+}
